2b320b4f9effa923be05e3ae973e1b262abfd166
Zaid Taiyab <ztaiyab@asu.edu>
2024-05-13 19:52:34 -0700

    Update data.h

--- data.h
+++ data.h
@@ -1,1 +1,27 @@
+#ifndef DATA_H
+#define DATA_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+#include "move.h"
+#include "pokemon.h"
+#include "json11.hpp"
+
+using namespace std;
+using json = json11::Json;
+
+/**
+* This function reads the json file and returns json data
+* @param file_path
+* @return json data
+*/
+
+json read_json_file(const string &file_path);
+vector<Move> loadMoves(string filename);
+vector<unique_ptr<Pokemon>> loadPokemon(string filename);
+
+#endif
